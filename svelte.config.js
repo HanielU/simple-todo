ccfb6c73a428a87f403ee12a980b2301bef79776
Haniel Ubogu <hanieltobi@gmail.com>
2023-08-04 23:23:33 +0100

    fix adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import sequence from "svelte-sequential-preprocessor";
 import { preprocessMeltUI } from "@melt-ui/pp";
 import { vitePreprocess } from "@sveltejs/kit/vite";
